dfd1671d74b394c2b6b39df0cefd7c41435c9931
Basheer Ahamed <consultant.ahamed@gmail.com>
2022-07-20 09:28:25 +0530

    CRUD

--- src/NotFound.js
+++ src/NotFound.js
@@ -1,12 +1,14 @@
-import React from 'react';
+import React from "react";
 
 export function NotFound() {
   return (
     <div className="not-found">
-       <h1>404 Page not found</h1>
-      <img className="not-found-pic"
-        src="https://media.istockphoto.com/vectors/concept-404-error-page-flat-cartoon-style-vector-illustration-vector-id1149316411?k=20&m=1149316411&s=612x612&w=0&h=wzSCBQVVh76LWzeEQP01DDEhpm983Y6_tsqlZ46goZ0=" alt="404 error! page not found" />
-     
+      <h1>404 Page not found</h1>
+      <img
+        className="not-found-pic"
+        src="https://media.istockphoto.com/vectors/concept-404-error-page-flat-cartoon-style-vector-illustration-vector-id1149316411?k=20&m=1149316411&s=612x612&w=0&h=wzSCBQVVh76LWzeEQP01DDEhpm983Y6_tsqlZ46goZ0="
+        alt="404 error! page not found"
+      />
     </div>
   );
 }
